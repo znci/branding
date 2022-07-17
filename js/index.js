c14bfa8c5e7e300056768f7c0397708085c4df41
zNotChill <59340653+zNotChill@users.noreply.github.com>
2022-07-17 12:46:50 +0100

    Add Emoji Support (might not work)

---
+++ js/index.js
@@ -0,0 +1,59 @@
+document.body.innerHTML = document.body.innerHTML.replace(":skull:", `<img class="reaction-emoji-1" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":dead:", `<img class="reaction-emoji-1" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":nerd:", `<img class="reaction-emoji-2" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":sunglasses:", `<img class="reaction-emoji-3" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":swag:", `<img class="reaction-emoji-3" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":cry:", `<img class="reaction-emoji-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":crying:", `<img class="reaction-emoji-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":yawn:", `<img class="reaction-emoji-5" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":tired:", `<img class="reaction-emoji-5" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":laughing:", `<img class="reaction-emoji-6" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":party:", `<img class="reaction-emoji-7" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":sad:", `<img class="reaction-emoji-8" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":monocle:", `<img class="reaction-emoji-9" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":looking:", `<img class="reaction-emoji-9" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":love:", `<img class="reaction-emoji-10" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":heart:", `<img class="reaction-emoji-10" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":poo:", `<img class="reaction-emoji-11" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":shit:", `<img class="reaction-emoji-11" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":nauseated:", `<img class="reaction-emoji-12" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":vomit:", `<img class="reaction-emoji-13" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":puke:", `<img class="reaction-emoji-13" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":sick:", `<img class="reaction-emoji-13" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":devil:", `<img class="reaction-emoji-14" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":evil:", `<img class="reaction-emoji-14" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":demon:", `<img class="reaction-emoji-14" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":clown:", `<img class="reaction-emoji-15" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":hiding:", `<img class="reaction-emoji-16" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":eyes:", `<img class="reaction-emoji-17" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":brain:", `<img class="reaction-emoji-18" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":genius:", `<img class="reaction-emoji-18" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":smart:", `<img class="reaction-emoji-18" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":thumbsup:", `<img class="reaction-emoji-19" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":like:", `<img class="reaction-emoji-19" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":thumbup:", `<img class="reaction-emoji-19" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":thumbsdown:", `<img class="reaction-emoji-20" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":dislike:", `<img class="reaction-emoji-20" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":thumbdown:", `<img class="reaction-emoji-20" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+
+document.body.innerHTML = document.body.innerHTML.replace(":smile:", `<img class="reaction-emoji-21" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
+document.body.innerHTML = document.body.innerHTML.replace(":happy:", `<img class="reaction-emoji-21" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></img>`);
